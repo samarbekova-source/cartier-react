c5d24000725fafa5dd9259e66f85d6ca3ede0c93
samarbekova-source <chikasamarbekova@gmail.com>
2022-04-04 17:52:03 +0600

    button

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -49,7 +49,7 @@ const Details = () => {
           >
             {checkItem ? "remove from cart" : "Add to cart"}
           </Button>
-          <Button>Buy now</Button>
+          <Button>Buy nowfrdxrgfhcvghgjsss</Button>
           <div>{oneJewelry.description}</div>
         </div>
       </div>
